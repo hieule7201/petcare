95184a34b0cedd86e14fa5853be30d0766172740
unknown <hieule7201@gmail.com>
2023-10-13 12:20:03 +0700

    set up folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,21 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      const res = await axios.get("http://localhost:8081/");
-      setData(res.data);
-    };
-    getData();
-  }, []);
-
-  console.log(data);
   return (
-    <div>
-      {data.map((params, index) => {
-        return (
-          <div key={index}>
-            <p>{params.name}</p>
-            <span>{params.value}</span>
-          </div>
-        );
-      })}
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
